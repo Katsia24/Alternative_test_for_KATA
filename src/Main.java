5c9a672346ba80ec9158baf8b60919a1eddb59fc
Kate <katerinanizh@gmail.com>
2024-04-03 20:18:16 +0300

    Проброска исключений

--- src/Main.java
+++ src/Main.java
@@ -41,7 +41,11 @@ public class Main {
         }
 
         Element element = new Element(elements[0], elements[2], elements[1]);
-        return element.calculation();
+        try {
+            return element.calculation();
+        } catch (IOException e) {
+            return "throws Exception //т.к. формат математической операции не удовлетворяет заданию";
+        }
     }
 }
 class Element {
@@ -54,84 +58,58 @@ class Element {
         this.b = b;
         this.el = el;
     }
-    String calculation() {
+    String calculation() throws IOException {
         String result = "";
         int n2 = 0;
         if(a.indexOf("\"") == -1){
+                throw new IOException(); //нету кавычек у первого элемента
+        }
+        a = a.substring(a.indexOf("\"")+1, a.lastIndexOf("\""));
+        if(a.length() > 10 || b.length() > 10){
+            throw new IOException(); //Строка не длинее 10 символов
+        }
+        if(b.indexOf("\"") == -1){
+            int bInt = 0;
             try {
-                throw new Exception();
-            } catch (Exception e) {
-                return  "нету кавычек у первого элемента";
+                bInt = Integer.parseInt(b);
+            }catch (NumberFormatException exception) {
+                throw new IOException(); //Первый элемент строка, а второй не строка и не число
             }
+            if(!(bInt >= 1 && bInt <= 10)){
+                throw new IOException(); //Ввод только от 1 до 10
+            }
+            switch (el) {
+                case ("*"):
+                    for(int i = 0; i < bInt; i++){
+                        result = result + a;
+                    }
+                    if(result.length() > 40){
+                        result = result.substring(0,40) + "...";
+                    }
+                    break;
+                case ("/"):
+                    result = a.substring(0,a.length() / bInt);
+                    break;
+                default:
+                    throw new IOException(); //Не найден элемент * /
+            }
+            return "\"" + result + "\"";
         }else {
-            a = a.substring(a.indexOf("\"")+1, a.lastIndexOf("\""));
-            if(a.length() > 10 || b.length() > 10){
-                try {
-                    throw new Exception();
-                } catch (Exception e) {
-                    return  "Строка не длинее 10 символов";
-                }
-            }else {
-                if(b.indexOf("\"") == -1){
-                    int bInt = 0;
-                    try {
-                        bInt = Integer.parseInt(b);
-                    }catch (NumberFormatException exception) {
-                        return  "Первый элемент строка, а второй не строка и не число";
-                    }
-                    if(!(bInt >= 1 && bInt <= 10)){
-                        try {
-                            throw new IOException() ;
-                        } catch (IOException e) {
-                            return  "Ввод только от 1 до 10";
-                        }
-                    }
-                    switch (el) {
-                        case ("*"):
-                            for(int i = 0; i < bInt; i++){
-                                result = result + a;
-                            }
-                            if(result.length() > 40){
-                                result = result.substring(0,40) + "...";
-                            }
-                            break;
-                        case ("/"):
-                            result = a.substring(0,a.length() / bInt);
-                            break;
-                        default:
-                            try {
-                                throw new IOException() ;
-                            } catch (IOException e) {
-                                System.out.println("Не найден элемент * /");
-                                System.exit(0);
-                            }
-                            break;
-                    }
-                    return "\"" + result + "\"";
+            b = b.substring(b.indexOf("\"")+1, b.lastIndexOf("\""));
+        }
+        switch (el) {
+            case ("+"):
+                result = a + b;
+                break;
+            case ("-"):
+                if(a.indexOf(b) == -1){
+                    result = a;
                 }else {
-                    b = b.substring(b.indexOf("\"")+1, b.lastIndexOf("\""));
+                    result = a.substring(a.indexOf(a), a.indexOf(b));
                 }
-                switch (el) {
-                    case ("+"):
-                        result = a + b;
-                        break;
-                    case ("-"):
-                        if(a.indexOf(b) == -1){
-                            result = a;
-                        }else {
-                            result = a.substring(a.indexOf(a), a.indexOf(b));
-                        }
-                        break;
-                    default:
-                        try {
-                            throw new IOException() ;
-                        } catch (IOException e) {
-                            System.out.println("Не найден элемент + -");
-                            System.exit(0);
-                        }
-                        break;
-                }
-            }
+                break;
+            default:
+                throw new IOException(); //Не найден элемент + -
         }
         return "\"" + result + "\"";
     }
